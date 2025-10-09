5a0e2179affeb7db363e133942d59195283b6dae
Precious Adedokun <apreezofficial@gmail.com>
2025-10-09 15:40:41 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import Hero from './HeroMain';
+import NavBar from './NavBar';
+import AcademicDifference from './AcademyDifference';
 export default function Home() {
   return (
+    <NavBar />
     <Hero />
+    <AcademicDifference />
   );
 }
